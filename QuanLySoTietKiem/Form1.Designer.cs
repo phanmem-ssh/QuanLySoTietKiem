53c03d38cc66556a434133808bbab417e24e559a
Hoàng Dr <drhoang999@gmail.com>
2019-05-17 16:10:12 +0700

    NHan,h 4

--- QuanLySoTietKiem/Form1.Designer.cs
+++ QuanLySoTietKiem/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(198, 100);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Sỹ 3";
+            this.button1.Text = "Sỹ 4";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
