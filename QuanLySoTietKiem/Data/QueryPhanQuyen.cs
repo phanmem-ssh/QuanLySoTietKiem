16b315c41631a664bb51cb21160b0e3e0e190b94
nguyenhongson1999 <17520989@gm.uit.edu.vn>
2019-05-31 13:14:58 +0700

    them file phanquyen

---
+++ QuanLySoTietKiem/Data/QueryPhanQuyen.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+
+namespace QuanLySoTietKiem.Data
+{
+    class QueryPhanQuyen
+    {
+        public static int ThemNhomNguoiDung(Object.objPhanQuyen pq)
+        {
+            int ok = -1;
+            SqlConnection cnn = Data.DataUtis.GetDBConnection();
+            cnn.Open();
+            try
+            {
+                string sql = "Insert into NHOM_NGUOI_DUNG (MaNhomNguoiDung, TenNhomNguoiDung) "
+                + " values () ";
+
+                SqlCommand cmd = cnn.CreateCommand();
+                cmd.CommandText = sql;
+
+                //DateTime myDate = DateTime.ParseExact(kh.getNgayCap(), "dd/MM/yyyy", null);
+
+                // Thêm tham số @MaKH
+                cmd.Parameters.Add("@MaNhomNguoiDung", SqlDbType.VarChar).Value = pq.getManhom();
+                cmd.Parameters.Add("@TenNhomNguoiDung", SqlDbType.NVarChar).Value = pq.getTenNhom();
+                
+
+                // Thực thi Command (Dùng cho delete, insert, update).
+                ok = cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Lỗi: " + e);
+            }
+            finally
+            {
+                cnn.Close();
+                cnn.Dispose();
+                cnn = null;
+            }
+            return ok;
+        }
+        public static void ThemNhanVien(string Bang, string Item)
+        {
+
+        }
+        public static void Sua()
+        {
+
+        }
+        public static DataTable LayDuLieu(string Bang)
+        {
+            SqlConnection cnn = Data.DataUtis.GetDBConnection();
+            cnn.Open();
+            string sql = "select * from " + Bang;
+            SqlCommand com = new SqlCommand(sql, cnn);
+            com.CommandType = System.Data.CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            cnn.Close();
+            return dt;
+        }
+        public static void Xoa()
+        {
+
+        }
+    }
+}
+}
---
+++ QuanLySoTietKiem/Object/objPhanQuyen.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySoTietKiem.Object
+{
+    class objPhanQuyen
+    {
+        public string MaNhomNguoiDung;
+        public string TenNhomNguoiDung;
+        public string MaQuyen;
+        public string TenQuyen;
+        public string MaNV;
+        public string MatKhau;
+        public string TenDangNhap;
+        public string getManhom()
+        {
+            return MaNhomNguoiDung;
+        }
+
+        public void setMaNhom(string MaNhomNguoiDung)
+        {
+            this.MaNhomNguoiDung = MaNhomNguoiDung;
+        }
+        public string getTenNhom()
+        {
+            return TenNhomNguoiDung;
+        }
+
+        public void setTenNhom(string TenNhomNguoiDung)
+        {
+            this.TenNhomNguoiDung = TenNhomNguoiDung;
+        }
+        public string getMaQuyen()
+        {
+            return MaQuyen;
+        }
+
+        public void setMaQuyen(string MaQuyen)
+        {
+            this.MaQuyen = MaQuyen;
+        }
+        public string getTenQuyen()
+        {
+            return TenQuyen;
+        }
+
+        public void setTebQuyen(string TenQuyen)
+        {
+            this.TenQuyen = TenQuyen;
+        }
+        public string getMaNV()
+        {
+            return MaNV;
+        }
+
+        public void setMaNV(string MaNV)
+        {
+            this.MaNV = MaNV;
+        }
+        public string getMatKhau()
+        {
+            return MatKhau;
+        }
+
+        public void setMatKhau(string MatKhau)
+        {
+            this.MatKhau = MatKhau;
+        }
+        public string getTenDangNhap()
+        {
+            return TenDangNhap;
+        }
+
+        public void setTenDangNhap(string TenDangNhap)
+        {
+            this.TenDangNhap = TenDangNhap;
+        }
+
+    }
+}
