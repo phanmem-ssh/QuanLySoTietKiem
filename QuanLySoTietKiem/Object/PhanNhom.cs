e1a7026e372e397e360635947811b4d95f63e1c0
Sỹ Hoàng Tiến <drhoang999@gmail.com>
2019-05-31 15:03:06 +0700

    ok Code Phan Nhom, cập nhật thêm

---
+++ QuanLySoTietKiem/Object/PhanNhom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySoTietKiem.Object
+{
+    class PhanNhom
+    {
+        string maNV;
+        string maNhomNguoiDung;
+
+        public string MaNV { get => maNV; set => maNV = value; }
+        public string MaNhomNguoiDung { get => maNhomNguoiDung; set => maNhomNguoiDung = value; }
+    }
+}
--- QuanLySoTietKiem/UcPhanNhom.cs
+++ QuanLySoTietKiem/UcPhanNhom.cs
@@ -15,6 +15,19 @@ namespace QuanLySoTietKiem
         public UcPhanNhom()
         {
             InitializeComponent();
+            DataTable dataPN = Data.Query.LayDuLieu("PHAN_NHOM");
+            DataTable dataNV = Data.Query.LayDuLieu("NHAN_VIEN");
+            dataGridPhanNhom.DataSource = dataPN;
+   
+            for (int i=0;i< dataPN.Rows.Count;i++)
+            {
+                dataGridPhanNhom.Rows[i].Cells[0].Value = 
+                    Data.Query.TenTraVe(dataPN.Rows[i].ItemArray[0].ToString(), "MaNV", "PHAN_NHOM", "NHAN_VIEN", "HoTen");
+                dataGridPhanNhom.Rows[i].Cells[1].Value =
+                    Data.Query.TenTraVe(dataPN.Rows[i].ItemArray[1].ToString(), "MaNhomNguoiDung", "PHAN_NHOM", "NHOM_NGUOI_DUNG", "TenNhomNguoiDung");
+
+            }
+
         }
     }
 }
