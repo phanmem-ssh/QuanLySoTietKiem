1a9b1ac085435fa8f7799c1fb875002581962856
Sỹ Hoàng Tiến <drhoang999@gmail.com>
2019-06-07 13:42:11 +0700

    hay

--- QuanLySoTietKiem/UcQuyen.cs
+++ QuanLySoTietKiem/UcQuyen.cs
@@ -34,5 +34,69 @@ namespace QuanLySoTietKiem
 
             quyen.XuatNhapLieu(ref txtMaQuyen, ref txtTenQuyen);
         }
+        private void LamMoi()
+        {
+            dataGridQuyen.DataSource = Data.Query.LayDuLieu("QUYEN");
+            dataGridQuyen.Update();
+        }
+        private void BtnThem_Click(object sender, EventArgs e)
+        {
+            btnLuu.Visible = true;
+            btnHuy.Visible = true;
+            btnSua.Visible = false;
+            btnXoa.Visible = false;    
+        }
+        private void BtnSua_Click(object sender, EventArgs e)
+        {
+            btnLuu.Visible = true;
+            btnHuy.Visible = true;
+            btnThem.Visible = false;
+            btnXoa.Visible = false;
+
+        }
+        private void BtnLuu_Click(object sender, EventArgs e)
+        {
+            if(btnSua.Visible == false)
+            {
+                Object.Quyen quyen = new Object.Quyen();
+                quyen.MaQuyen = txtMaQuyen.Text;
+                quyen.TenQuyen = txtTenQuyen.Text;
+                quyen.ThemData();
+            }
+
+            if (btnThem.Visible == false)
+            {
+                Object.Quyen quyen = new Object.Quyen();
+                quyen.MaQuyen = txtMaQuyen.Text;
+                quyen.TenQuyen = txtTenQuyen.Text;
+                quyen.SuaData();
+            }
+            LamMoi();
+        }
+
+        private void BtnHuy_Click(object sender, EventArgs e)
+        {
+            btnSua.Visible = true;
+            btnThem.Visible = true;
+            btnXoa.Visible = true;
+            btnLuu.Visible = false;
+            btnHuy.Visible = false;
+        }
+
+        private void BtnXoa_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Bạn thực sự muốn xóa?", "Cảnh báo!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Object.Quyen quyen = new Object.Quyen();
+                quyen.MaQuyen = txtMaQuyen.Text;
+                quyen.XoaData();
+                LamMoi();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+
+            }
+        }
     }
 }
